363909b5e9f5bbcba2d0db6642a9ecbee03f64f8
Kaushal112411 <Kaushal112411@gmai.com>
2022-05-15 07:19:56 +0530

    mern

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(bodyParser.json({limit: "30mb",extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb",extended: true}));
 app.use(cors());
 app.use('/posts',postroutes);
+app.get('/',(req,res)=>{
+    res.send('hello to memories api')
+})
 
 const CONNECTION_URL = 'mongodb+srv://unsorted_array:112411Sp@cluster0.yacbp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
