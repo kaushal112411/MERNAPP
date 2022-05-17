7a7fb37de73ad0362e86879c5d40bf188f620591
MUNDE SANJAY <38632534+im-crooked@users.noreply.github.com>
2022-05-17 20:00:14 +0530

    updated file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import postroutes from "routes/posts.js";
+import postroutes from "./routes/posts";
 import dotenv from "dotenv";
 
 const app = express();
